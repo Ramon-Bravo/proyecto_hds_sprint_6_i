98b888499b132ae1a6029f5493adab99771acaac
Ramon-Bravo <113270267+Ramon-Bravo@users.noreply.github.com>
2024-10-16 19:28:37 -0600

    Código de aplicación Web

---
+++ app.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('C:/Users/packe/OneDrive/Documentos/proyecto_sprint_6/vehicles_us.csv') # leer los datos
+
+st.header("Analisis de datos Proyecto 6")
+
+hist_button = st.button('Construir histograma') # crear un botón
+
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Construir Scatter') # crear un botón
+
+if scatter_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un gráfico de disperción para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
