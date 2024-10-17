230bd17b2f0f1eac826edd41487db457d033f756
Ramon-Bravo <113270267+Ramon-Bravo@users.noreply.github.com>
2024-10-16 20:46:23 -0600

    cambio de ruta al archivo csv

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('C:/Users/packe/OneDrive/Documentos/proyecto_sprint_6/vehicles_us.csv') # leer los datos
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 
 st.header("Analisis de datos Proyecto 6")
 
